396c42937fa264f9a88ff044799812fd3a185e27
dawood-bits <daudjan.jfd@gmail.com>
2025-09-16 10:30:51 -0400

    Today 16 Sep

--- AMSWebSimple/obj/Debug/net8.0/AMSWebSimple.AssemblyInfo.cs
+++ AMSWebSimple/obj/Debug/net8.0/AMSWebSimple.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AMSWebSimple")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a27610bab2e2f43153e5665fb6732a627a219684")]
 [assembly: System.Reflection.AssemblyProductAttribute("AMSWebSimple")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AMSWebSimple")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
